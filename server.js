adc9f607066c541a78d23b2c1c4e2462ca687af4
Thodoris Thomaidis <thodoris.thomaidis@gmail.com>
2021-01-17 16:29:18 +0200

    changed server port/index

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-const io = require("socket.io")(3000, {
-    cors: {
-      origin: "*",
-    },
-  });
+const express = require('express');
+const socketIO = require('socket.io');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+const io = socketIO(server)
 
 const users = {}
 
