e52e850046687f8bbe2f4e05b2a4ff4a7498472a
Thodoris Thomaidis <thodoris.thomaidis@gmail.com>
2021-01-17 16:54:12 +0200

    server config

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = 'https://theo-chat-demo.herokuapp.com/';
+const INDEX = '/index.html';
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
